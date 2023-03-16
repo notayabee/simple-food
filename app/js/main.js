8bd24a8220a024203e3d1d0f32bfbeac1deb7934
Notayabee <1455tonny1455@gmail.com>
2023-03-16 20:12:41 +0300

    first homework

---
+++ app/js/main.js
@@ -0,0 +1,9 @@
+$(function() {
+    // $('').on('click', functio () {
+    //     $('').toggleClass('');
+    // });
+
+
+    var mixer = mixitup('.popular__main');
+    var mixer = mixitup('.menu__list');
+});
